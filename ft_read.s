400ee4d6695495d57eaf5f491302514a1009121b
nsimon <nsimon@student.42.fr>
2020-08-07 17:27:42 +0200

    set correct errno flag for ft_write and ft_read

--- ft_read.s
+++ ft_read.s
@@ -1,7 +1,16 @@
 section .text
-    global _ft_read
+    global  _ft_read
+    extern  ___error
 
 _ft_read:
     mov rax, 0x2000003
     syscall
+    jc  error
+    ret
+
+error:
+    mov r8, rax
+    call ___error
+    mov [rax], r8
+    mov rax, -1
     ret
